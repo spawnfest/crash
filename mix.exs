5ebd31617c596dd33bfc3875673ba13a7b50f7d0
lucazulian <lucagiuggia@gmail.com>
2021-09-18 08:27:34 +0200

    chore: add dev and test dependencies

--- mix.exs
+++ mix.exs
@@ -35,13 +35,28 @@ defmodule Crash.MixProject do
     [
       {:phoenix, "~> 1.5.12"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
+    ] ++ deps_dev()
+  end
+
+  defp deps_dev do
+    [
+      {:benchfella, "~> 0.3.0", only: [:dev, :test], runtime: false},
+      {:credo, "1.5.6", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: [:dev, :test]},
+      {:exprof, "~> 0.2.0", only: [:dev, :test], runtime: false},
+      {:floki, ">= 0.30.0", only: :test},
+      {:observer_cli, "~> 1.6", only: [:test, :dev]},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:propcheck, "~> 1.1", only: [:test, :dev]},
+      {:muzak, "~> 1.1", only: :test}
     ]
   end
 
@@ -53,7 +68,8 @@ defmodule Crash.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "cmd npm install --prefix assets"]
+      setup: ["deps.get", "cmd npm install --prefix assets"],
+      "format.all": "format mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}' 'config/*.{ex,exs}'"
     ]
   end
 end
