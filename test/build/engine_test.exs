b4633ec10ce80f04325e5071f866913b3e4da120
lucazulian <lucagiuggia@gmail.com>
2021-09-19 22:52:36 +0200

    test: add engine tests

--- test/build/engine_test.exs
+++ test/build/engine_test.exs
@@ -28,4 +28,38 @@ defmodule Crash.Build.EngineTest do
       end
     end
   end
+
+  describe "builds/0" do
+    test "can get all builds from an existing Engine" do
+      with_mock Supervisor, start_remote_job: fn _, _ -> {:ok, :c.pid(0, 250, 0)} end do
+        engine_pid = start_supervised!(Engine)
+
+        pipeline = PipelineBuilder.build()
+        repository = RepositoryBuilder.build()
+
+        assert {:ok, build} = Engine.schedule(pipeline, repository)
+
+        send(engine_pid, {:update, self(), %{build: build}})
+
+        assert [^build] = Engine.builds()
+      end
+    end
+  end
+
+  describe "info/1" do
+    test "can get a single build from an existing Engine" do
+      with_mock Supervisor, start_remote_job: fn _, _ -> {:ok, :c.pid(0, 250, 0)} end do
+        engine_pid = start_supervised!(Engine)
+
+        pipeline = PipelineBuilder.build()
+        repository = RepositoryBuilder.build()
+
+        assert {:ok, build} = Engine.schedule(pipeline, repository)
+
+        send(engine_pid, {:update, self(), %{build: build}})
+
+        assert ^build = Engine.info(build.id)
+      end
+    end
+  end
 end
